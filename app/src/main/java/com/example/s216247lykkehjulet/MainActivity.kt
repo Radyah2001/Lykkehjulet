f635dab7a0636deeab880bcc313867611a34f4a2
Radyah2001 <haydar919@gmail.com>
2022-11-15 13:37:18 +0100

    Github repo start

---
+++ app/src/main/java/com/example/s216247lykkehjulet/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.s216247lykkehjulet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.s216247lykkehjulet.ui.theme.S216247LykkehjuletTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            S216247LykkehjuletTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    S216247LykkehjuletTheme {
+        Greeting("Android")
+    }
+}
